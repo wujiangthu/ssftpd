dae6051304d06b8e1f4a4284cf690ad38c2f5922
Wu Jiang <wujaingthu@gmail.com>
2012-05-08 00:26:35 +0800

    add some ftp reply code.

--- src/ftp/ss_ftp_reply.h
+++ src/ftp/ss_ftp_reply.h
@@ -30,8 +30,14 @@ void ss_ftp_reply(ss_ftp_request *r, const char *reply_code);
  *  Positive Completion Replies  
  */
 
+#define COMMAND_OK                 "200 Command ok.\r\n" 
+#define COMMAND_NOT_IMPLEMENTED    "202 Command not implemented.\r\n" 
+#define SERVICE_READY              "220 Service is ready.\r\n" 
 #define USER_LOGGED_IN             "230 Logged in.\r\n" 
-
+#define USER_LOGGED_OUT            "231 Logged out.\r\n" 
+#define USER_DELAYED_LOGGED_OUT    "232 Logged out command received,   \ 
+                                        will complete when transfer done.\r\n" 
+#define PATH_CREATED               "257 Path created.\r\n"    
 
 /* 
  *  3XX
@@ -40,6 +46,7 @@ void ss_ftp_reply(ss_ftp_request *r, const char *reply_code);
  */
 
 #define USER_NAME_OK_NEED_PASSWORD "331 Need password.\r\n" 
+#define NEED_ACCOUNT               "332 Need account.\r\n" 
 
 
 /*
@@ -48,6 +55,9 @@ void ss_ftp_reply(ss_ftp_request *r, const char *reply_code);
  *  Transient Negative Replies  
  */
 
+#define INVALID_USERNAME_PASSWORD  "430 Invalid username or password.\r\n" 
+#define HOST_UNAVAILABLE           "434 Requested host unavailable.\r\n" 
+
 
 /*
  *  5XX
@@ -55,6 +65,8 @@ void ss_ftp_reply(ss_ftp_request *r, const char *reply_code);
  *  Permanent Negative Completion Replies  
  */
 
+#define COMMAND_SYNTAX_ERROR       "500 Syntax error,command unrecognized.\r\n" 
+#define ARGUMENT_SYNTAX_ERROR      "501 Arguments syntax error.\r\n" 
 
 
 #endif /* _SS_FTP_REPLY_H_  */
