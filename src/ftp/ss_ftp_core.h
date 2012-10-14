f61b5df50cf3b8f64091549408d2a2de39f9447c
Wu Jiang <wujaingthu@gmail.com>
2012-10-14 22:59:20 +0800

    add all ftp related head files into one headers.

---
+++ src/ftp/ss_ftp_core.h
@@ -0,0 +1,32 @@
+#ifndef _SS_FTP_CORE_H_
+#define _SS_FTP_CORE_H_
+
+
+#include <ngx_config.h>
+#include <ngx_core.h>
+#include <ngx_event.h>
+#include <assert.h>
+
+#include "lib/ss_file.h"
+#include "lib/ss_string.h"
+#include "lib/ss_memory.h"
+#include "ss_ftp_cmd.h"
+#include "ssftp.h"
+#include "lib/ss_time.h"
+#include "ss_ftp_reply.h"
+#include "ss_ftp_read.h"
+#include "ss_ftp_write.h"
+#include "ss_ftp_parse.h"
+#include "ss_ftp_ssl.h"
+#include "ss_ftp_pam.h"
+
+#include <stddef.h>
+#include <dirent.h>
+#include <sys/types.h>
+
+#include <security/pam_appl.h>
+#include <security/pam_misc.h>
+
+
+
+#endif /* _SS_FTP_CORE_H_ */
