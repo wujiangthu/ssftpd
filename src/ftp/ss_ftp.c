7b47fe885ddeddba84948c2155fe5c02fac1b7cd
Wu Jiang <wujaingthu@gmail.com>
2012-10-17 02:44:57 +0800

    bug fix

--- src/ftp/ss_ftp.c
+++ src/ftp/ss_ftp.c
@@ -13,6 +13,9 @@
 static char *ss_ftp_block(ngx_conf_t *cf, ngx_command_t *cmd, void *conf);
 
 
+ngx_uint_t  ss_ftp_max_module;
+
+
 static ngx_command_t ss_ftp_commands[] = {
 
   { ngx_string("ftp"),
@@ -51,7 +54,6 @@ ss_ftp_block(ngx_conf_t *cf, ngx_command_t *cmd, void *conf)
 {
    char                        *rv;
    ngx_uint_t                   mi, m;
-   ngx_uint_t                   ngx_http_max_module;
    ngx_conf_t                   pcf;
 //   ngx_http_module_t           *module;
    ss_ftp_module_t           *module;
@@ -77,20 +79,20 @@ ss_ftp_block(ngx_conf_t *cf, ngx_command_t *cmd, void *conf)
  
    /* count the number of the http modules and set up their indices */
  
-   ngx_http_max_module = 0;
+   ss_ftp_max_module = 0;
    for (m = 0; ngx_modules[m]; m++) {
        if (ngx_modules[m]->type != SS_FTP_MODULE) {
           continue;
        }
  
-       ngx_modules[m]->ctx_index = ngx_http_max_module++;
+       ngx_modules[m]->ctx_index = ss_ftp_max_module++;
    }
  
  
    /* the ftp main_conf context, it is the same in the all ftp contexts */
  
    ctx->main_conf = ngx_pcalloc(cf->pool,
-                                sizeof(void *) * ngx_http_max_module);
+                                sizeof(void *) * ss_ftp_max_module);
    if (ctx->main_conf == NULL) {
       return NGX_CONF_ERROR;
    }
@@ -101,7 +103,7 @@ ss_ftp_block(ngx_conf_t *cf, ngx_command_t *cmd, void *conf)
     * the server{}s' srv_conf's
     */
  
-   ctx->srv_conf = ngx_pcalloc(cf->pool, sizeof(void *) * ngx_http_max_module);
+   ctx->srv_conf = ngx_pcalloc(cf->pool, sizeof(void *) * ss_ftp_max_module);
    if (ctx->srv_conf == NULL) {
        return NGX_CONF_ERROR;
    }
@@ -158,6 +160,11 @@ ss_ftp_block(ngx_conf_t *cf, ngx_command_t *cmd, void *conf)
    cf->cmd_type = SS_FTP_MAIN_CONF;
    rv = ngx_conf_parse(cf, NULL);
  
+printf("***********------------------****\n");
+ss_ftp_core_main_conf_t *mm=ctx->main_conf[ss_ftp_core_module.ctx_index];
+ss_ftp_core_srv_conf_t  *ss=mm->servers.elts;
+printf("%u\n", ss->port);
+
    if (rv != NGX_CONF_OK) {
        goto failed;
    }
@@ -221,6 +228,8 @@ ss_ftp_block(ngx_conf_t *cf, ngx_command_t *cmd, void *conf)
  
    *cf = pcf;
  
+//printf("%u\n",  ((ss_ftp_core_srv_conf_t *) (ctx->main_conf[ss_ftp_core_module.ctx_index])->servers->elts))->port);
+
  
 //   if (ngx_http_init_phase_handlers(cf, cmcf) != NGX_OK) {
 //       return NGX_CONF_ERROR;
